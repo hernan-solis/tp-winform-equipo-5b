b527fdd9ab891c31320e966eb8ee01b2972eabcf
Hache94k <hernansolis94k@gmail.com>
2025-09-01 20:24:23 -0300

    Agregar Ventana Principal y primeros cambios

--- TPWinForm/TPWinForm/frmVentanaPrincipal.Designer.cs
+++ TPWinForm/TPWinForm/frmVentanaPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TPWinForm
 {
-    partial class Form1
+    partial class frmVentanaPrincipal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmVentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(932, 547);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmVentanaPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ventana Principal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
--- TPWinForm/TPWinForm/frmVentanaPrincipal.cs
+++ TPWinForm/TPWinForm/frmVentanaPrincipal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TPWinForm
 {
-    public partial class Form1 : Form
+    public partial class frmVentanaPrincipal : Form
     {
-        public Form1()
+        public frmVentanaPrincipal()
         {
             InitializeComponent();
         }
