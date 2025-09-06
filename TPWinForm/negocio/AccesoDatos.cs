ea5159fafd3be3352d01b891a1050d245ac4600b
brunobberti <brunoberti094@gmail.com>
2025-09-06 10:06:03 -0300

    Creacion clase ArticuloNegocio (vacia) Creacion clase AccesoDatos con los atributos de conexion, comando y lector. Y el constructor.

---
+++ TPWinForm/negocio/AccesoDatos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using dominio;
+
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public AccesoDatos() //constructor 
+        {
+            conexion = new SqlConnection(""); //falta enviar la ruta por parametro
+            comando = new SqlCommand();
+        }
+
+    }
+}
